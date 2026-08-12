990ee9c902e27e0b6c687555e45d2e41651353b4
meheraj786 <meherajhosen786@gmail.com>
2026-08-12 21:23:52 +0600

    docker files add

--- product-service/src/prisma/prisma.service.ts
+++ product-service/src/prisma/prisma.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+
 import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '../generated/prisma/client';
 import pg from 'pg';
 
 @Injectable()
