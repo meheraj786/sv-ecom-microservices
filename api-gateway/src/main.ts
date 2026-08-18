fa3039c395da40cabaa8881b19abd88981fa5cb6
meheraj786 <meherajhosen786@gmail.com>
2026-08-18 15:06:03 +0600

    cookie related bug fix in api gateway

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
+import { TransformInterceptor } from './common/interceptors/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://tiny-url-psi.vercel.app'],
+    credentials: true,
+  });
+
+  app.use(cookieParser());
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+  app.useGlobalInterceptors(new TransformInterceptor());
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`API Gateway is running on port ${port}`);
 }
 bootstrap();
